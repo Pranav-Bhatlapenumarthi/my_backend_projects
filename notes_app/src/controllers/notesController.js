a765c2362ec742d855fac75e52396d06f8029b89
Sujatha Bhatlapenumarthi <bhpranav18@gmail.com>
2025-12-15 23:08:05 +0530

    update: notesController implemented

--- notes_app/src/controllers/notesController.js
+++ notes_app/src/controllers/notesController.js
@@ -1,3 +1,4 @@
+const { model, get } = require("mongoose");
 const Note = require("../models/noteModel");
 
 const createNote = async (req, res, next) =>{
@@ -18,8 +19,50 @@ const createNote = async (req, res, next) =>{
 
 const listNotes = async (req, res, next) =>{
     try{
-        const notes = await Note.find({owner: req.user._id}).sort({updatedAt: -1});
+        const notes = await Note.find({owner: req.user._id}).sort({updatedAt: -1}); // orders the results by the timestamp in descending order (newest note first)
+        return res.json(notes);
     } catch(err){
         next(err);
     }
 };
+
+const getNote = async (req, res, next) => {
+    try{
+        const note = await Note.findOne({ _id: req.params.id, owner: req.user._id}); // req.params gives the named route parameters
+        if (!note){
+            return res.status(404).json({message: "Note not found"});
+        }
+        return res.status(200).json(note);
+    } catch(err){
+        next(err);
+    }
+};
+
+const deleteNote = async (req, res, next) => {
+    try{
+        const note = await Note.findOneAndDelete({_id: req.params.id, owner: req.user._id});
+        if(!note){
+            return res.status(404).json({message: "Note not found"});
+        }
+
+        return res.status(200).json({message: "Note deleted successfully"});
+    } catch(err){
+        next(err);
+    }
+};
+
+const updateNote = async (req, res, next) =>{
+    try{
+        const update = req.body;
+        const note = await Note.findOneAndUpdate({_id: req.params.id, owner: req.user._id, }, update, {new: true});
+        if(!note){
+            return res.status(404).json({message: "Note not found"});
+        }
+
+        return res.status(200).json(note);
+    } catch(err){
+        next(err);
+    }
+};
+
+module.exports = {createNote, getNote, deleteNote, updateNote, listNotes};
