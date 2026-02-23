b42eec79baf89abac82480824daf5c7cf5d84fd9
Pranav Bhatlapenumarthi <bhpranav18@gmail.com>
2026-02-23 17:18:13 +0530

    chore: minor fixes

--- auth_service_from_scratch/server.js
+++ auth_service_from_scratch/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = reuuire("cors");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes.js");
 
