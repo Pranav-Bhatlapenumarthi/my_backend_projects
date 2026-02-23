aeddc315f1dc8faad8479ddd185c825dedf32aa4
Pranav Bhatlapenumarthi <bhpranav18@gmail.com>
2026-02-23 22:46:28 +0530

    started auth logic

--- auth_service_from_scratch/controllers/authController.js
+++ auth_service_from_scratch/controllers/authController.js
@@ -1,2 +1,28 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+const generateToken = (userId) => {
+    return jwt.sign(
+        {id: userId},
+        process.env.JWT_SECRET,
+        {expiresIn: JWT_EXPIRES_IN}
+    );
+};
+
+const register = async(req, res) => {
+    try{
+        const {name, email, password} = req.body;
+
+        const userExits = await User.findOne({email});
+        if(userExits){
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            });
+        }
+
+    } catch (error){
+
+    }
+};
