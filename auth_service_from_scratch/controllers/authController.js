2990b326dc132b10d5ddc30c169e4aa8a3d236c6
Pranav Bhatlapenumarthi <bhpranav18@gmail.com>
2026-02-24 17:15:29 +0530

    auth logic ready with login and register features

--- auth_service_from_scratch/controllers/authController.js
+++ auth_service_from_scratch/controllers/authController.js
@@ -10,11 +10,12 @@ const generateToken = (userId) => {
     );
 };
 
+// route - POST /api/auth/register
 const register = async(req, res) => {
     try{
         const {name, email, password} = req.body;
 
-        const userExits = await User.findOne({email});
+        const userExits = await User.findOne({email}); // A Promise object is truthy
         if(userExits){
             return res.status(400).json({
                 success: false,
@@ -22,7 +23,78 @@ const register = async(req, res) => {
             });
         }
 
-    } catch (error){
+        const salt = await bcrypt.genSalt(12);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
+        const user = await User.create({ // Creating user
+            name: name,
+            email:email,
+            password: hashedPassword
+        });
+
+        const token = generateToken(user._id); // _id is a primary key in the MongoDB
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+
+    } catch (error){
+        console.error("Registration error: ", error);
+        res.status(500).json({
+            success: false,
+            message: "Error occured while user registration"
+        })
+    } 
+    
+};
+
+// route - POST /api/auth/login
+const login = async(req, res) => {
+    try{
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ 
+                success: false,
+                message: 'Invalid credentials' 
+            });
+        }
+
+        const token = generateToken(user._id);
+        res.status(201).json({
+            success: true,
+            message: "Login Successfull",
+            token,
+            user:{
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+
+    } catch(error){
+        console.error('Login error:', error);
+        res.status(500).json({ 
+            success: false,
+            message: 'Server error during login' 
+        });
     }
 };
+
+module.exports = { register, login };
